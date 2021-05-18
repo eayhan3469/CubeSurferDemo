d8410f6f1eef743fc5dcdef57d725400975dc143
Egemen Ayhan <eayhan3469@gmail.com>
2021-05-19 01:45:06 +0300

    .

--- CubeSurferDemo/Assets/Scripts/CameraController.cs
+++ CubeSurferDemo/Assets/Scripts/CameraController.cs
@@ -5,33 +5,22 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private Transform Target;
+    private Transform camTarget;
 
-    public float SmoothFactor;
+    [SerializeField]
+    private Transform lookTarget;
 
-    private Vector3 cameraOffset;
+    [SerializeField]
+    private float smoothSpeed = 10f;
 
-    private void Start()
-    {
-        cameraOffset = transform.position - Target.position;
-    }
+    [SerializeField]
+    private Vector3 distance;
 
     private void LateUpdate()
     {
-        if (Target != null)
-        {
-            if (4 > 1) //TODO: karakter yüksekliği ayarlanacak
-            {
-                Vector3 newPosition = (Target.position + cameraOffset) - new Vector3(0f, 0f, 0f);// *;/*+ (Target.localScale * 0.2f)*/; //TODO: topuk sayısına göre uzaklık değişecek
-                transform.position = Vector3.Slerp(transform.position, newPosition, SmoothFactor);
-            }
-            else
-            {
-                Vector3 newPosition = (Target.position + cameraOffset);// *;/*+ (Target.localScale * 0.2f)*/; //TODO: topuk sayısına göre uzaklık değişecek
-                transform.position = Vector3.Slerp(transform.position, newPosition, SmoothFactor);
-            }
-
-            transform.rotation = Quaternion.Slerp(transform.rotation, Target.rotation, SmoothFactor * Time.deltaTime);
-        }
+        Vector3 dPos = camTarget.position + distance;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, smoothSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
     }
 }
