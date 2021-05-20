cf67d8017ebf64c73be1b75265f0d4d6febff948
Egemen Ayhan <eayhan3469@gmail.com>
2021-05-20 23:12:52 +0300

    Cube collect finished.

--- CubeSurferDemo/Assets/Scripts/Character.cs
+++ CubeSurferDemo/Assets/Scripts/Character.cs
@@ -10,20 +10,33 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    public static Character Instance { get; private set; }
+    [SerializeField]
+    private Transform cubesParent;
 
+    [SerializeField]
+    private GameObject cubeObject;
+
+    public static Character Instance { get; private set; }
     public GameObject CharacterObject
     {
         get { return characterObject; }
     }
-
     public bool IsRotating { get; set; }
     public int Direction { get; set; }
-
     public float Speed
     {
         get { return speed; }
         set { speed = value; }
+    }
+
+    public List<Cube> Cubes;
+    private BoxCollider _characterCollider;
+
+    private void Start()
+    {
+        _characterCollider = gameObject.GetComponent<BoxCollider>();
+        Cubes.AddRange(GetComponentsInChildren<Cube>());
+        ResizeCollider();
     }
 
     void Awake()
@@ -32,5 +45,31 @@ public class Character : MonoBehaviour
             Debug.LogError("'Character' must be one!");
         else
             Instance = this;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "CollectableCube")
+        {
+            AddCube();
+            Destroy(collision.gameObject);
+            Debug.Log("1");
+        }
+    }
+
+    public void AddCube()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 100f);
+        ResizeCollider();
+        var cubePosition = new Vector3(0f, Cubes[Cubes.Count - 1].transform.localPosition.y - cubeObject.transform.localScale.y, 0f);
+        var cube = Instantiate(cubeObject, cubesParent);
+        Cubes.Add(cube.GetComponent<Cube>());
+        cube.transform.localPosition = cubePosition;
+    }
+
+    public void ResizeCollider()
+    {
+        _characterCollider.size = new Vector3(_characterCollider.size.x, _characterCollider.size.y + 1f, _characterCollider.size.z);
+        _characterCollider.center = new Vector3(_characterCollider.center.x, _characterCollider.center.y - 0.5f, _characterCollider.center.z);
     }
 }
